8cba176b0d9048b61540ec55d6df116604d25566
jdawson <jonathan.dawson789@mgmail.com>
2022-05-12 06:57:03 -0700

    add heroku psql url

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///inventory_app'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'postgresql:///flask-heroku', 'postgresql:///inventory_app' )
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
